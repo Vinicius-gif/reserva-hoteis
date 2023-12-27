246b908560a59ce3f7034c743e1f4c1327e64bfa
Vinicius <vfpv@hotmail.com>
2023-12-26 21:26:01 -0300

    add image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Image from 'next/image';
+
+import hoteis from '../data/hoteis.json';
+
 export default function Home() {
-  return <main>Helo World!</main>;
+  return (
+    <main>
+      Helo World!
+      <Image src={hoteis[0].imagens[0]} alt="hotel" width={100} height={100} />
+    </main>
+  );
 }
