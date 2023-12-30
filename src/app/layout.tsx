31f77ed9ec6af41d0487e31b1822981c36bb828d
Vinicius <vfpv@hotmail.com>
2023-12-29 21:37:26 -0300

    config html element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-lt-installed={true}>
       <body className={inter.className}>
         <Header />
         {children}
